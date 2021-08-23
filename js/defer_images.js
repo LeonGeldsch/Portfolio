61a9a2e562304a3c4dd709e2967583e5a7cb8c65
LeonGeldsch <leon.geldschlaeger@gmail.com>
2021-08-23 09:59:31 +0200

    added image deferring for slideshow

---
+++ js/defer_images.js
@@ -0,0 +1,18 @@
+var frontImages = document.querySelectorAll('.left img, .active img, .right img');
+var backImages = document.querySelector('.hidden img');
+
+function loadImages() {
+    for (let i = 0; i < frontImages.length; i++) {
+        if (frontImages[i].getAttribute('data-src')) {
+            frontImages[i].setAttribute('src', frontImages[i].getAttribute('data-src'));
+        }
+    }
+    for (let i = 0; i < backImages.length; i++) {
+        if (backImages[i].getAttribute('data-src')) {
+            backImages[i].setAttribute('src', backImages[i].getAttribute('data-src'));
+        }
+    }
+}
+
+
+window.onload = loadImages();
